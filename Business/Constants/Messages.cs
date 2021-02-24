cfa25414f2c2a06995ba7bb4052c8865eb318c6b
Onur Demir <Onur.d77@outlook.com>
2021-02-25 00:20:33 +0300

    Business rules added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
 		public static string ProductNameInvalid = "Ürün ismi geçersizke";
 		public static string MaintenanceTime = "Sistem bakımda";
 		public static string ProductsListed = "Ürünler listelendi";
+		public static string ProductCountOfCategoryError = "Bu kategoride en fazla 10 ürün olabilir";
+		public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+		public static string CategoryLimitExceded = "Kategori sayısı çok fazla!";
 	}
 }
