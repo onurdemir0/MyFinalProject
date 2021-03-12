0b1fd9cb31435eeffc74f7faf9748926e8d9ff33
Onur Demir <Onur.d77@outlook.com>
2021-03-12 21:14:41 +0300

    Caching, Transaction and Performance layers added!

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,8 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConserns.Validation;
 using Core.Utilities.Business;
@@ -30,6 +32,7 @@ namespace Business.Concrete
 
 		[SecuredOperation("product.add,admin")]
 		[ValidationAspect(typeof(ProductValidator))]
+		[CacheRemoveAspect("IProductService.Get")]
 		public IResult Add(Product product)
 		{
 			IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
@@ -45,20 +48,22 @@ namespace Business.Concrete
 			return new SuccessResult(Messages.ProductAdded);
 		}
 
+		[CacheAspect]
 		public IDataResult<List<Product>> GetAll()
 		{
-			if (DateTime.Now.Hour == 23)
-			{
-				return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-			}
+			//if (DateTime.Now.Hour == 23)
+			//{
+			//	return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+			//}
 			return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
 		}
 
-		public IDataResult<List<Product>> GetAllByCategoryId(int id)
+		public IDataResult<List<Product>> GetAllByCategoryId(int id) 
 		{
 			return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
 		}
 
+		[CacheAspect]
 		public IDataResult<Product> GetById(int productId)
 		{
 			return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -79,6 +84,7 @@ namespace Business.Concrete
 		}
 
 		[ValidationAspect(typeof(ProductValidator))]
+		[CacheRemoveAspect("IProductService.Get")]
 		public IResult Update(Product product)
 		{
 			throw new NotImplementedException();
@@ -110,6 +116,12 @@ namespace Business.Concrete
 				return new ErrorResult(Messages.CategoryLimitExceded);
 			}
 			return new SuccessResult();
+		}
+
+		[TransactionScopeAspect]
+		public IResult AddTransactionalTest(Product product)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
